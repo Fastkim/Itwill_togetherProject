891ad8806b9500ca8cdca9a1acfaec4224484d07
young <0loveyo0602@gmail.com>
2022-12-02 18:15:16 +0900

    Merge branch 'master' of https://github.com/KAYOUNG62/project5.git

--- src/main/java/com/example/project5/controller/MemberController.java
+++ src/main/java/com/example/project5/controller/MemberController.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
+@RequiredArgsConstructor 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
--- src/main/java/com/example/project5/controller/RecruitPostController.java
+++ src/main/java/com/example/project5/controller/RecruitPostController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.project5.domain.RecruitPost;
@@ -43,10 +45,10 @@ public class RecruitPostController {
     
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/post/create")
-    public String create(RecruitPostCreateDto dto, RedirectAttributes attrs) {
+    public String create(RecruitPostCreateDto dto, RedirectAttributes attrs, @RequestParam("imgFile") MultipartFile poster ) throws Exception {
         log.info("create(dto={})" , dto);
         
-        RecruitPost entity = recruitPostService.create(dto);
+        RecruitPost entity = recruitPostService.create(dto, poster);
         
         attrs.addFlashAttribute("createId", entity.getId());
         
