e6227a59a198070aa79e23f7b4cd3c4f93446388
young <0loveyo0602@gmail.com>
2022-12-02 11:40:40 +0900

    .

--- src/main/java/com/example/project5/controller/RecruitPostController.java
+++ src/main/java/com/example/project5/controller/RecruitPostController.java
@@ -2,7 +2,6 @@ package com.example.project5.controller;
 
 import java.util.List;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
