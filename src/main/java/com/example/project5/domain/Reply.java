770db7fefb61d83a1556823e719ed716b12622c3
kayoung <0loveyo0602@gmail.com>
2022-12-04 17:48:52 +0900

    댓글기능 작업중

--- src/main/java/com/example/project5/domain/Reply.java
+++ src/main/java/com/example/project5/domain/Reply.java
@@ -1,5 +1,13 @@
 package com.example.project5.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,12 +19,23 @@ import lombok.ToString;
 @Builder
 @Getter
 @ToString
+@Entity(name = "POSTREPLIES")
+@SequenceGenerator(name="POSTREPLIES_SEQ_GEN", sequenceName = "POSTREPLIES_SEQ" ,allocationSize = 1)
 public class Reply extends BaseTimeEntity {
     
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "POSTREPLIES_SEQ_GEN")
     private Integer id;
+	
+	@ManyToOne
     private String replyContent;
+	
+	@Column(nullable = false)
     private Integer postId;
+	
     private String postGroup;
+
+    @Column(nullable = false)
     private String writer;
     
     public Reply updateReply(String replyContent) {
