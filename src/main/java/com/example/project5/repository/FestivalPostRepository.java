7345a305dcffcf9980a546fee0cacecb97f9eda7
김다훈(DA HUN KIM) <109465725+Fastkim@users.noreply.github.com>
2022-12-13 14:20:59 +0900

    paging
    paging

--- src/main/java/com/example/project5/repository/FestivalPostRepository.java
+++ src/main/java/com/example/project5/repository/FestivalPostRepository.java
@@ -9,27 +9,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.project5.domain.FestivalPost;
-import com.example.project5.domain.FreeSharePost;
 
 // JpaRepository<엔터티 클래스 타입, PK 데이터 타입>을 상속하는 인터페이스를 선언.
 
 public interface FestivalPostRepository extends JpaRepository<FestivalPost, Integer> {
-
+    
+    
+    Page<FestivalPost> findAll(Pageable pageable);
     // select * from COMMUNITYS order by ID desc
     List<FestivalPost> findByOrderByIdDesc();
-    Page<FestivalPost> findAll(Pageable pageable);
     
- // 제목 검색:
+    
+    // 제목 검색:
     // select * from POSTS where lower(TITLE) like lower(?) order by ID desc
     List<FestivalPost> findByTitleIgnoreCaseContainingOrderByIdDesc(String title);
     
     // 내용 검색:
     // select * from POSTS where lower(CONTENT) like lower(?) order by ID desc
     List<FestivalPost> findByContentIgnoreCaseContainingOrderByIdDesc(String content);
-    
-    // 작성자 검색:
-    // select * from POSTS where lower(AUTHOR) like lower(?) order by ID desc
-    List<FestivalPost> findByAuthorIgnoreCaseContainingOrderByIdDesc(String author);
     
     // 제목 + 내용 검색:
     // select * from POSTS 
@@ -39,10 +36,10 @@ public interface FestivalPostRepository extends JpaRepository<FestivalPost, Inte
     
     // JPQL(Java Persistence Query Language)
     @Query(
-        "select c from COMMUNITYS c "
-            + " where lower(c.title) like lower('%' || :keyword || '%') "
-            + " or lower(c.content) like lower('%' || :keyword || '%') "
-            + " order by c.id desc"
+        "select p from COMMUNITYS p "
+            + " where lower(p.title) like lower('%' || :keyword || '%') "
+            + " or lower(p.content) like lower('%' || :keyword || '%') "
+            + " order by p.id desc"
     )
     List<FestivalPost> searchByKeyword(@Param(value = "keyword") String keyword);
 
