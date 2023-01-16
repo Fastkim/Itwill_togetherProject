c68eee9de371010db60512e23aa789291a2d6e4d
young <0loveyo0602@gmail.com>
2023-01-16 18:19:17 +0900

    지도 수정 및 채팅 기능 작업중

--- src/main/java/com/example/project5/dto/ChatDto.java
+++ src/main/java/com/example/project5/dto/ChatDto.java
@@ -19,7 +19,7 @@ public class ChatDto {
     
     private MessageType type;
     private String roomId; // 방번호
-    private String senderId; // 채팅을 보낸 사람
+    private String sender; // 채팅을 보낸 사람
     private String message; // 메시지 내용
     private String time; // 메시지 발송 시간
     
